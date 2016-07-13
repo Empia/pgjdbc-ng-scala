8fc6e8e19c67c1a92dd8907b2518e90c4114656a
Stan <iamjacke@gmail.com>
2016-07-13 15:37:53 +0500

    Testing jdbc driver

--- build.sbt
+++ build.sbt
@@ -5,6 +5,10 @@ settings(
   scalaVersion := "2.11.7",
   mainClass := Some("Main"),
 
-   libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.0"
+resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+
+
+   libraryDependencies += "com.impossibl.pgjdbc-ng" % "pgjdbc-ng" % "0.7.0-SNAPSHOT"
+   //libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.0"
    //libraryDependencies += "org.apache.spark" %% "spark-mllib" % "1.5.0",
 )
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,12 +1,44 @@
 package basic
+package jdbc
 
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
+import java.sql.DriverManager
+import java.sql.Connection
 
+/**
+ * A Scala JDBC connection example by Alvin Alexander,
+ * http://alvinalexander.com
+ */
 object Main {
-  //val sc = 
-  def main(args: Array[String]) = {
-    println("Hello World")
+
+  def main(args: Array[String]) {
+    // connect to the database named "mysql" on the localhost
+    val driver = "com.impossibl.postgres.jdbc.PGDriver"
+    val url = "jdbc:pgsql://localhost/corpreg"
+    val username = "postgres"
+    val password = "12344321"
+
+    // there's probably a better way to do this
+    var connection:Connection = null
+
+    try {
+      // make the connection
+      Class.forName(driver)
+      connection = DriverManager.getConnection(url, username, password)
+
+      // create the statement, and run the select query
+      val statement = connection.createStatement()
+      val resultSet = statement.executeQuery("SELECT * FROM \"user\"")
+      while ( resultSet.next() ) {
+        val host = resultSet.getString("email")
+        val user = resultSet.getString("fullName")
+        println("host, user = " + host + ", " + user)
+      }
+    } catch {
+      case e => e.printStackTrace
+    }
+    connection.close()
   }
+
 }
+
+
